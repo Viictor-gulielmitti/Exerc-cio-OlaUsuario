252ad819c1e0441868a9afac5e8857d60198c80f
Viictor-gulielmitti <79719548+Viictor-gulielmitti@users.noreply.github.com>
2021-03-14 18:14:34 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ola_Usuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {   string nome;
+            Console.WriteLine("Digite seu nome");
+            nome = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write($"Olá {nome}");
+        }
+    }
+}
